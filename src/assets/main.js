d531bb93d498c4c662edb1c736b6ce09da9f8f4e
Luis Angel Perez Melendez <angelmelen88@gmail.com>
2022-12-15 15:16:24 -0600

    pagina 2.0

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,1 +1,44 @@
 // UC5OwlwvsVmf_lXHguB6OYFw - kard
+
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UC5OwlwvsVmf_lXHguB6OYFw&part=snippet%2Cid&order=date&maxResults=10';
+
+const content = null || document.getElementById('content');
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '4cd44e51f4mshe0be12a099a0788p16aeabjsn21095175bb05',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlApi) {
+  const response = await fetch(urlApi, options);
+  const data = await response.json();
+  return data;
+}
+
+(async () => {
+  try {
+    const videos = await fetchData(API);
+    let view = `
+    ${videos.items.map(video => `
+      <div class="group relative">
+        <div
+          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+        </div>
+        <div class="mt-4 flex justify-between">
+          <h3 class="text-sm text-gray-700">
+            <span aria-hidden="true" class="absolute inset-0"></span>
+            ${video.snippet.title}
+          </h3>
+        </div>
+      </div>
+    `).slice(0, 4).join('')}
+    `;
+    content.innerHTML = view;
+  } catch (error) {
+    console.log(error);
+  }
+})();
